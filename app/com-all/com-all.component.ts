fba19876d0e17e476d7d6740ed9ef7b4ded5e5e6
kxm21390 <44552588+kxm21390@users.noreply.github.com>
2019-02-07 22:20:20 -0800

    Update com-all.component.ts

--- app/com-all/com-all.component.ts
+++ app/com-all/com-all.component.ts
@@ -10,7 +10,22 @@ export class ComAllComponent implements OnInit {
 
   constructor(private router: Router) { }
   public urlData;
+  public prices;
+  public dropdownValues;
   ngOnInit() {
     this.urlData = this.router.url.slice(1);
+    this.prices = ['$', '$$', '$$$'];
+    if(this.urlData === 'Food'){
+      this.dropdownValues = ['SURPRISE ME', 'AMERICAN', 'BURGERS', 'CHINESE', 'MEXICAN', 'PIZZA', 'SANDWICHES', 'SUSHI'];
+    }
+    else if(this.urlData === 'Drinks'){
+      this.dropdownValues = ['SURPRISE ME', 'COFFEE', 'JUICE & SMOOTHIES', 'BEER GARDEN', 'COCKTAIL BAR', 'LOUNGES', 'SPORTS BAR', 'WINE BAR']
+    }
+    else {
+      this.dropdownValues = ['SURPRISE ME', 'SHOOPING', 'TOURS', 'FITNESS', 'WINERIES', 'FARMS', 'ARTS', 'FASHION', 'NIGHTLIFE', 'ACTIVE', 'FESTIVALS'];
+    }
   }
+
+
+  
 }
